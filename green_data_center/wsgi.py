d85c92a22885612a028cc20050cc26fe7a6f5e02
aboulouafa <107998621+abou57mehdi@users.noreply.github.com>
2024-08-08 13:47:45 +0100

    Update wsgi.py

--- green_data_center/wsgi.py
+++ green_data_center/wsgi.py
@@ -14,4 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'green_data_center.settings')
 
 application = get_wsgi_application()
-app = appliccation
